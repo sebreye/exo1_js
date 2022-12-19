8c6e7a221b3d862c089f3f87a27cf856a5d9d70d
sebastian Reyes <sebreyes222gmail.com>
2022-12-19 11:17:38 +0100

    version final

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+// partie 1
+let ma_phrase = "J'aime les tartines et j'en mange"
+console.log(ma_phrase)
+let nombre_par_jours = "4 par jours"
+console.log(nombre_par_jours)
+console.log(ma_phrase + nombre_par_jours)
+let trois = 5
+console.log(trois)
+let quatre = 35
+console.log(quatre)
+let resultat = trois + quatre;
+console.log(resultat)
+let resultat2 = ma_phrase+" "+trois
+console.log(resultat2)
+// partie 2
+let phrase1 = "Salut tout le monde"
+console.log(phrase1)
+let phrase2 = 'Au revoir !'
+console.log(phrase2)
+phrase1=phrase2
+console.log(phrase1)
+// partie 3
+let chiffre1 = 4;
+console.log(chiffre1)
+let chiffre2 = "6";
+console.log(chiffre2)
+chiffre1 > chiffre2
+console.log(chiffre1>chiffre2)
+console.log(chiffre1<chiffre2)
+console.log(chiffre1==chiffre2)
+console.log(chiffre1!=chiffre2)
+console.log(chiffre1===chiffre2)
+console.log(chiffre1!==chiffre2)
